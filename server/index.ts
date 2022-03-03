877710a6963cbce833ba33ecaf60bd926438579b
Konsta Purtsi <konsta@identio.fi>
2022-03-03 16:34:34 +0200

    Initial grapqhl server.

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,32 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+import people from './data.json';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const schema = `
+  type Query {
+    people: [Person!]
+  }
+
+  type Person {
+    id: ID!
+    name: String!
+    favoriteFood: String!
+  }
+`;
+
+const root = {
+  people: () => people
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(schema),
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!');
